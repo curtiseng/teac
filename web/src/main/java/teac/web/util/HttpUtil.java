2f3742b1812182f0399df19eae5ed6cd57e62506
yangzifeng <qq1406233473@gmail.com>
2016-12-20 22:16:43 +0800

    协作开发

--- web/src/main/java/teac/web/util/HttpUtil.java
+++ web/src/main/java/teac/web/util/HttpUtil.java
@@ -10,9 +10,6 @@ public class HttpUtil {
 
     public static boolean isAjaxRequest(HttpServletRequest request) {
 
-        if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
-            return true;
-        }
-        return false;
+        return "XMLHttpRequest".equals(request.getHeader("X-Requested-With"));
     }
 }
